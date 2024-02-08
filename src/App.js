6b311c7135e87412249be385afd9329e307eaa61
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-02-08 18:09:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+export default App;
+
+const options = ["Nie", "Dlaczego nie?", "Błagam?", "Nie rób mi tego"];
+const options2 = ["Tak", "Tak", "Tak", "Nahh ima do my own thing"];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
     </div>
   );
 }
 
-export default App;
+function Main() {
+  const [yesPressed, setYesPressed] = useState(false);
+  if (yesPressed)
+    return (
+      <main>
+        <div className="gif">
+          <img src="https://media1.tenor.com/m/ju1buBTwt64AAAAC/spiderverse-across-the-spiderverse.gif" />
+        </div>
+        <h1 className="text">Lets gooo! To bedziemy my 🥹</h1>
+      </main>
+    );
+
+  return (
+    <main>
+      <div className="gif">
+        <img
+          width={300}
+          height={300}
+          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzBjejdoeHczOTBtcWdleDhlYzB6YXp5dWdlNGlvcHAzYnFsbGF4NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/X23pjaOFiyH3G/giphy.gif"
+        />
+      </div>
+      <h1 className="text">Zostaniesz moją walentynką?</h1>
+      <div className="btns">
+        <Button yesPressed={yesPressed} setYesPressed={setYesPressed} />
+      </div>
+    </main>
+  );
+}
+
+function Button({ yesPressed, setYesPressed }) {
+  const [topSize, setTopSize] = useState(1);
+  const [leftSize, setLeftSize] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  function onNo() {
+    const movingleft = Math.min(Math.random() * 500) + -300;
+    const movingright = Math.min(Math.random() * 500) + -300;
+    setTopSize(topSize + movingright);
+    setLeftSize(leftSize + movingleft);
+
+    if (currentIndex < options.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  }
+
+  return (
+    <>
+      <button className="yes" onClick={() => setYesPressed(true)}>
+        Tak
+      </button>
+      <button
+        className="no"
+        onMouseOver={onNo}
+        style={{ left: `${leftSize}px`, top: `${topSize}px` }}
+      >
+        {options[currentIndex]}
+      </button>
+    </>
+  );
+}
